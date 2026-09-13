3c217e712694f49d99a49d4d4d2413e0b0024374
mmoraentregacr-a11y <mmora.entregacr@gmail.com>
2026-09-13 11:18:14 -0600

    Retry SQL connection after transient failure

--- api/src/db.ts
+++ api/src/db.ts
@@ -5,9 +5,11 @@ let pool: Promise<sql.ConnectionPool> | undefined;
 export function database() {
   const connectionString = process.env.SQL_CONNECTION_STRING;
   if (!connectionString) throw new Error("SERVER_NOT_CONFIGURED");
-  pool ||= sql.connect(connectionString);
+  pool ||= sql.connect(connectionString).catch(error => {
+    pool = undefined;
+    throw error;
+  });
   return pool;
 }
 
 export { sql };
-
